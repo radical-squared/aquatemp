e562bd0152ec4f0e5a14a368493bbd1f1360d039
Danne <danne@getduff.com>
2021-04-09 14:04:16 +0200

    some updates

--- control.sh
+++ control.sh
@@ -6,29 +6,44 @@ mqttpass=`cat $path/settings | grep mqttpass | cut -f2`
 mqttport=`cat $path/settings | grep mqttport | cut -f2`
 name=`cat $path/settings | grep hassname | cut -f2`
 
+check()
+{
+$path/heatpump info
+sleep 1
+}
+
 mosquitto_sub -v -R -h $mqtt -p $mqttport -u $mqttuser -P $mqttpass -t homeassistant/# | while read line
 do
-	settemp=`echo $line | grep "$name"_settemp | rev | cut -d ' ' -f1 | rev`
-	mode=`echo $line | grep "$name"_mode_set | rev | cut -d ' ' -f1 | rev`
-	silent=`echo $line | grep "$name"_silent | rev | cut -d ' ' -f1 | rev`
+	settemp=`echo $line | grep "$name"_settemp/state | rev | cut -d ' ' -f1 | rev`
+	mode=`echo $line | grep "$name"_mode_set/state | rev | cut -d ' ' -f1 | rev`
+	silent=`echo $line | grep "$name"_silent/set | rev | cut -d ' ' -f1 | rev`
 	if [ ! -z $settemp ]; then
 		$path/heatpump temp $settemp
+		echo $line
+		check
 	fi
 	if [ ! -z $silent ]; then
 		$path/heatpump silent $silent
+		echo $line
+		check
 	fi	
 	if [ ! -z $mode ]; then
+		echo $line
 		if [ $mode = "off" ]; then
 			$path/heatpump off
+			check
 		elif [ $mode = "heat" ]; then
 			$path/heatpump on
 			$path/heatpump mode heat
+			check
 		elif [ $mode = "auto" ]; then
 			$path/heatpump on
 			$path/heatpump mode auto
+			check
 		elif [ $mode = "cool" ]; then
 			$path/heatpump on
 			$path/heatpump mode cool
+			check
 		fi
 	fi
 
